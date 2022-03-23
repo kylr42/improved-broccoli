07bb7327eb42851266a0e67d7be51fee800f4c3e
MacBook <macbook@MacBook-Pro-MacBook.local>
2022-03-23 05:15:25 +0300

    resturing project structure

---
+++ app/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py flush --no-input
+python manage.py migrate
+python manage.py shell -c """
+from django.contrib.auth import get_user_model;
+get_user_model().objects.create_superuser(email='admin@gmail.com', password='password')
+"""
+
+exec "$@"
---
+++ app/transaction/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework.exceptions import ValidationError
+from rest_framework import serializers
+
+from .models import Transaction, PersonalAccount
+
+
+class PersonalAccountSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PersonalAccount
+        fields = ['id', 'customer', 'balance', 'is_active', ]
+        read_only_fields = ('id', 'balance', 'is_active')
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+
+    def validate(self, data):
+        if data['payer'] == data['payee']:
+            raise ValidationError({"payer": "Payer must not equal to payee"})
+        return super().validate(data)
+
+    def create(self, validated_data):
+        from core.tasks import transaction_handler
+
+        transfer = Transaction.objects.create(**validated_data)
+        transaction_handler.delay(
+            payer_id=validated_data['payer'].id,
+            payee_id=validated_data['payee'].id,
+            amount=validated_data['amount'],
+            transfer_id=transfer.id,
+        )
+        return transfer
+
+    class Meta:
+        model = Transaction
+        fields = ['id', 'payer', 'payee', 'amount', 'status', 'created']
+        read_only_fields = ('id', 'status', 'created')
+
